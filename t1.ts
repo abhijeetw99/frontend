835f767672bd131e0981924f9cd07c0866ce9f1b
Abhijeet N. Waghmare <awaghmare@dc2.propsol.org>
2020-03-08 20:58:43 +0530

    3/8 0858

--- t1.ts
+++ t1.ts
@@ -21,3 +21,56 @@ console.log('add=', getArith(4,3));
 const getEsArith = ( a:number, b:number) => a*b;
 
 console.log('es mul = ', getEsArith(3,5));
+
+console.log("Lexical This");
+
+var emp = {
+    id:1,
+    show:function(){
+        console.log( 'id = ',this.id );
+        setTimeout(() => {
+            console.log( 'timed out id = ',this.id );
+        }, 1000);
+    }
+};
+
+console.log(emp.show());
+
+// Rest ans spread
+
+console.log('Rest and spread');
+
+const restColors = function(message:string, ...colors:[]){
+    for( let i in colors ) {
+        console.log( colors[i] );
+    }
+}
+
+const spreadColors = function( ...colors:[]){
+    for(let i in colors){
+        console.log(colors[i]);
+    }
+}
+
+let message = "List of colors";
+
+restColors( message, 'red', 'blue' );
+
+const colors = ['white', 'silver'];
+
+spreadColors( ...colors);
+
+// For off loop
+
+console.log('For off loop');
+
+let animes = ['opm','bleach', 'MHA'];
+
+for( let i in animes ) {
+    console.log( animes[i] );
+}
+
+for( let anime of animes ) {
+    console.log( anime );
+}
+ 
